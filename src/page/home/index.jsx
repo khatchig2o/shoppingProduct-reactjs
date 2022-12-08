784043d897b9f48295cd489783839f0808854474
unknown <khatchigmaljiant@gmail.com>
2022-12-08 21:03:18 +0400

    fixed relodeand scroll bar  bug

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,54 +1,54 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Items from "../../components/items"
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function Home(){
-    const items = useSelector(state=>state.items.items)
-    const [activeTab,setActivetab]=useState("All")
-    const[filteredProducts,setfilteredProducts]= useState([])
+export default function Home() {
+    const items = useSelector(state => state.items.items)
+    const [activeTab, setActivetab] = useState("All")
+    const [filteredProducts, setfilteredProducts] = useState([])
     const dispatch = useDispatch()
-    const [navedat]=useState(["All","women's clothing","men's clothing","electronics"])
+    const [navedat] = useState(["All", "women's clothing", "men's clothing", "electronics"])
 
 
-    const getIndex=(index)=>{
+    const getIndex = (index) => {
         dispatch({
-            type:'ADD-item',
-            payload : items[index]
+            type: 'ADD-item',
+            payload: items[index]
         })
     }
-    useEffect(()=>{
+    useEffect(() => {
         selectProduct(activeTab)
-    },[])
+    }, [items])
     const selectProduct = (productName) => {
-        let arr= []
+        let arr = []
         if (productName !== 'All') {
-            arr=items.filter(x => x.category === productName)
+            arr = items.filter(x => x.category === productName)
         } else {
-            arr=items
+            arr = items
         }
         setfilteredProducts(arr)
-        setActivetab (productName)
+        setActivetab(productName)
     }
 
     return <section className='G-container'>
-            <div className='G-justify-center p-filter '>
-                <ul className='G-flex '>
-                    {navedat.map((nav,index)=>(
-                        <li key={index} className={(activeTab === nav ) ? 'active' : null}
-                            onClick={() => selectProduct(nav)}>{nav}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <div className='G-justify-center p-filter '>
+            <ul className='G-flex '>
+                {navedat.map((nav, index) => (
+                    <li key={index} className={(activeTab === nav) ? 'active' : null}
+                        onClick={() => selectProduct(nav)}>{nav}
+                    </li>
+                ))}
+            </ul>
+        </div>
         <div className='G-flex-wrap item-list'>
-            {filteredProducts.length ? filteredProducts.map((item,index)=>(
+            {filteredProducts.length ? filteredProducts.map((item, index) => (
                 <Items
                     item={item}
                     key={index}
                     nums={index}
                     addtocart={getIndex}
-            />))
-            : <p>loading ...</p>}
+                />))
+                : <p>loading ...</p>}
         </div>
     </section>
 }
